b927da149bcbcb5c7282d82e63367ba178268dbd
Andy Marks <amarks@thoughtworks.com>
2016-11-01 20:11:44 +1100

    Adding (coloured) logging to see order of Mongo interactions

--- src/clj/simtwop_web/db/core.clj
+++ src/clj/simtwop_web/db/core.clj
@@ -2,6 +2,7 @@
   (:require [monger.core :as mg]
   					[monger.query :as q]
   					[simtwop-web.config :refer [env]])
+  (:require [clojure.tools.logging :as log])
   (:require monger.joda-time)
   (:require [monger.collection :as mc]))
 
@@ -9,6 +10,7 @@
 (def db-url "mongodb://127.0.0.1/simtwop")
 
 (defn load-projects []
+	(log/info "Loading all projects...")
 	(let [{:keys [conn db]} (mg/connect-via-uri db-url)]
 		(q/with-collection db "projects"
 			(q/find {})
@@ -16,5 +18,6 @@
 			(q/sort (array-map :created-on -1)))))
 
 (defn save-project [project]
+	(log/info "Saving project...")
 	(let [{:keys [conn db]} (mg/connect-via-uri db-url)]
     (mc/insert-and-return db "projects" project)))
