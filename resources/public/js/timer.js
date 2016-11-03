1805effd4512f458ffd9e2c4969d1449717a4037
Andy Marks <amarks@thoughtworks.com>
2016-11-03 18:41:16 +1100

    Split out all timer related JS

---
+++ resources/public/js/timer.js
@@ -0,0 +1,27 @@
+function scoreProject() {
+  $('#projectScoring').modal();
+  $('#projectScoring').on('hidden.bs.modal', function (e) {
+    document.game.submit();
+  })
+}
+
+function handleTimerExpiry() {
+  selectedProjectRole = null;
+  $(PROJECT_ROLE_ROW).removeClass("project_role_selected_row");
+  scoreProject();
+}
+
+var clock = $('.counter').FlipClock(20, {
+    clockFace: 'Counter'
+  });
+
+  setTimeout(function() {
+    var interval = setInterval(function() {
+      clock.decrement();
+      if(clock.getTime().time <= 0) {
+        handleTimerExpiry();
+        // clock.stop();
+        // clearInterval(interval);
+      }     
+    }, 1000);
+  });
